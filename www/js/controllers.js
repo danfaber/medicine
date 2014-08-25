13e6766eeeedbf722920702f7558d816ba49a9d6
danfaber <danielguyfaber@gmail.com>
2014-08-25 13:24:29 +0100

    remove login screen code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,35 +1,27 @@
 
 var app = angular.module('medicine');
 
-/*
-angular.module('starter.controllers', [])*/
+app.controller('AppCtrl', function($scope, $ionicModal, $timeout, $rootScope) {
 
-  app
-
-
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $rootScope) {
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
+/*  // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
-  });
+  });*/
 
   // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
+/*  $scope.closeLogin = function() {
     $scope.modal.hide();
-  },
+  },*/
 
   // Open the login modal
-  $scope.login = function() {
+/*  $scope.login = function() {
     $scope.modal.show();
-  };
+  };*/
 
   // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
+/*  $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
@@ -37,16 +29,16 @@ angular.module('starter.controllers', [])*/
     $timeout(function() {
       $scope.closeLogin();
     }, 1000);
-  };
+  };*/
 
 
   $scope.backButtonClicked = function(){
       $rootScope.$broadcast("backButtonClicked",{});
   };
 
-})
+});
 
-.controller('PlaylistsCtrl', function($scope) {
+/*.controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
     { title: 'Chill', id: 2 },
@@ -65,8 +57,10 @@ angular.module('starter.controllers', [])*/
 
 
 
-})
+})*/
 
+/*
 .controller('PlaylistCtrl', function($scope, $stateParams) {
           $scope.typeId = $stateParams.typeId;
 })
+*/
