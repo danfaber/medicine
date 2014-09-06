414cbdd5e310ee5ad278a96137894f8068485a79
danfaber <danielguyfaber@gmail.com>
2014-09-06 15:31:36 +0100

    tidy up

--- www/js/controllers/exportController.js
+++ www/js/controllers/exportController.js
@@ -1,13 +1,24 @@
 (function(){
     var app = angular.module("medicine");
 
+    var currentDateTime;
+    var applicationName = "Log Book";
+    var gFileSystem;
+    var gFileEntry;
+
+
     app.controller("exportController", function($scope) {
+
+        function getFileName()
+        {
+            return applicationName + " " + currentDateTime+".html";
+        }
 
         $scope.exportData = function()
         {
 
-            alert("hi there!");
-            alert(cordova.file.dataDirectory);
+
+         //   alert(cordova.file.dataDirectory);
 
 
            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
@@ -22,7 +33,7 @@
             }
 
             function gotFileWriter(writer) {
-                writer.onwriteend = function(evt) {
+/*                writer.onwriteend = function(evt) {
                     alert("contents of file now 'some sample text'");
                     writer.truncate(11);
                     writer.onwriteend = function(evt) {
@@ -33,8 +44,8 @@
                             alert("contents of file now 'some different text'");
                         }
                     };
-                };
-                writer.write("some sample text");
+                };*/
+                writer.write("dan text here");
             }
 
             function fail(error) {
@@ -59,7 +70,7 @@
             function gotFile(file){
                 readDataUrl(file);
                 readAsText(file);
-              //  emailFile(file);
+                emailFile(file);
             }
 
             function readDataUrl(file) {
@@ -85,32 +96,6 @@
             }
         };
 
-
-        $scope.email = function()
-        {
-            alert("about to email");
-
-
-            window.plugins.socialsharing.shareViaEmail(
-                'Message goes here...', // can contain HTML tags, but support on Android is rather limited:  http://stackoverflow.com/questions/15136480/how-to-send-html-content-with-image-through-android-default-email-client
-                'Subject Dan',
-                ['danielguyfaber@gmail.com'], // TO: must be null or an array
-                null, // CC: must be null or an array
-                null, // BCC: must be null or an array
-                null, // FILES: can be null, a string, or an array
-                onSuccess, // called when sharing worked, but also when the user cancelled sharing via email (I've found no way to detect the difference)
-                onError // called when sh*t hits the fan
-            );
-
-            function onSuccess(){
-                alert("worked!");
-            }
-
-            function onError()
-            {
-                alert("failed!");
-            }
-        };
 
         function emailFile(url)
         {
