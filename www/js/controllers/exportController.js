12edf0f01f8367311c5b1d47999d1c6dc50b8dce
danfaber <danielguyfaber@gmail.com>
2014-09-06 16:26:23 +0100

    export working!

--- www/js/controllers/exportController.js
+++ www/js/controllers/exportController.js
@@ -3,18 +3,63 @@
 
     var currentDateTime;
     var applicationName = "Log Book";
-    var gFileSystem;
-    var gFileEntry;
-
-
-    function padToTwoDigits(number)
-    {
-        var numberString = number.toString();
-        return (numberString.length === 1) ? "0"+numberString : numberString;
-    }
-
 
     app.controller("exportController", function($scope) {
+
+        $scope.exportData = function()
+        {
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+
+            var fileSystem;
+            var fileEntry;
+
+            currentDateTime = new Date();
+
+            function gotFS(fs) {
+                fileSystem = fs;
+                fileSystem.root.getFile(getFileName(), {create: true, exclusive: false}, gotFileEntry, fail);
+            }
+
+            function gotFileEntry(fe) {
+                fileEntry = fe;
+                fileEntry.createWriter(gotFileWriter, fail);
+            }
+
+            function gotFileWriter(writer) {
+                writer.onwriteend = function(evt) {
+                    emailFile(fileEntry.toURL());
+                };
+
+                writer.write("<h1>Hello Louit!</h1><div>Just need to put data from the system in here which shouldn't be too hard :)</div><div>Gonna try to get that done tomorrow.</div><div>D</div>");
+            }
+
+            function fail(error) {}
+        };
+
+        function emailFile(url)
+        {
+            window.plugins.socialsharing.shareViaEmail(
+                'Good news pal.  This message has been automatically sent by our app.  Check out the attachment...', // can contain HTML tags, but support on Android is rather limited:  http://stackoverflow.com/questions/15136480/how-to-send-html-content-with-image-through-android-default-email-client
+                getFileName(),
+                ['danielguyfaber@gmail.com'], // TO: must be null or an array
+                null, // CC: must be null or an array
+                null, // BCC: must be null or an array
+                url, // FILES: can be null, a string, or an array
+                onSuccess, // called when sharing worked, but also when the user cancelled sharing via email (I've found no way to detect the difference)
+                onError // called when sh*t hits the fan
+            );
+
+            function onSuccess(){}
+
+            function onError(){}
+        }
+
+        function padToTwoDigits(number)
+        {
+            var numberString = number.toString();
+            return (numberString.length === 1) ? "0"+numberString : numberString;
+        }
+
 
         function getFileName()
         {
@@ -25,96 +70,6 @@
                 + padToTwoDigits(currentDateTime.getHours()) + "."
                 + padToTwoDigits(currentDateTime.getMinutes())
                 +".html";
-        }
-
-        $scope.exportData = function()
-        {
-           window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-
-            currentDateTime = new Date();
-
-            function gotFS(fileSystem) {
-                gFileSystem = fileSystem;
-                gFileSystem.root.getFile(getFileName(), {create: true, exclusive: false}, gotFileEntry, fail);
-            }
-
-            function gotFileEntry(fileEntry) {
-                gFileEntry = fileEntry;
-                gFileEntry.createWriter(gotFileWriter, fail);
-            }
-
-            function gotFileWriter(writer) {
-                writer.onwriteend = function(evt) {
-                    emailFile(gFileEntry.toURL());
-                };
-
-/*                writer.onwriteend = function(evt) {
-                    alert("contents of file now 'some sample text'");
-                    writer.truncate(11);
-                    writer.onwriteend = function(evt) {
-                        alert("contents of file now 'some sample'");
-                        writer.seek(4);
-                        writer.write(" different text");
-                        writer.onwriteend = function(evt){
-                            alert("contents of file now 'some different text'");
-                        }
-                    };
-                };*/
-                writer.write("<h1>Heading</h1><div>Maybe this will work</div>");
-            }
-
-            function fail(error) {
-                alert(error.code);
-            }
-        };
-
-        $scope.readData = function()
-        {
-
-            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-
-            function gotFS(fileSystem) {
-                fileSystem.root.getFile(getFileName(), null, gotFileEntry, fail);
-            }
-
-            function gotFileEntry(fileEntry) {
-                fileEntry.file(gotFile, fail);
-                emailFile(fileEntry.toURL());
-            }
-
-            function gotFile(file){
-                emailFile(file);
-            }
-
-            function fail(evt) {
-                alert(evt.target.error.code);
-            }
-        };
-
-
-        function emailFile(url)
-        {
-            alert(url);
-
-            window.plugins.socialsharing.shareViaEmail(
-                'Message goes here...', // can contain HTML tags, but support on Android is rather limited:  http://stackoverflow.com/questions/15136480/how-to-send-html-content-with-image-through-android-default-email-client
-                'Subject Dan',
-                ['danielguyfaber@gmail.com'], // TO: must be null or an array
-                null, // CC: must be null or an array
-                null, // BCC: must be null or an array
-                url, // FILES: can be null, a string, or an array
-                onSuccess, // called when sharing worked, but also when the user cancelled sharing via email (I've found no way to detect the difference)
-                onError // called when sh*t hits the fan
-            );
-
-            function onSuccess(){
-                alert("worked!");
-            }
-
-            function onError()
-            {
-                alert("failed!");
-            }
         }
 
     });
